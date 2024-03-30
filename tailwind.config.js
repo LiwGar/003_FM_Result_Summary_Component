aef26873c76afaaaf8e629836ca6bb2d40ae7e7e
liwgar <wilenri341@hotmail.com>
2024-03-29 20:08:50 -0500

    Configurando fonts y colors para el proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
-  ],
-  theme: {
-    extend: {},
+export const content = [
+  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './components/**/*.{html,js}',
+  './src/**/*.{html,js,ts,jsx,tsx,mdx}',
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      haken: ["Hanken Grotesk", 'sans-serif']
+    },
+    colors: {
+      primary: {
+        LightRed: 'hsl(0, 100%, 67%)',
+        OrangeYellow: 'hsl(39, 100%, 56%)',
+        GreenTeal: 'hsl(166, 100%, 37%)',
+        CobaltBlue: 'hsl(234, 85%, 45%)',
+      },
+      gradients: {
+        LightSlateBlue: 'hsl(252, 100%, 67%)', //(background)
+        LightRoyalBlue: 'hsl(241, 81%, 54%)',
+        VioletBlue: 'hsla(256, 72%, 46%, 1)', //(circle)
+        PersianBlue: 'hsla(241, 72%, 46%, 0)',
+      },
+      neutral: {
+        White: 'hsl(0, 0%, 100%)',
+        PaleBlue: 'hsl(221, 100%, 96%)',
+        LightLavender: 'hsl(241, 100%, 89%)',
+        DarkGrayBlue: 'hsl(224, 30%, 27%)',
+      }
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
